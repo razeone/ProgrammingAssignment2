d0d0471176e720b09df6b83cca8c0b8f708bc4af
Jorge Raze <jorge.raze@clevertech.biz>
2016-11-08 02:05:55 -0600

    Add comments and update readme

--- code.R
+++ code.R
@@ -1,4 +1,9 @@
+## Tests
+## Loading the functions
 source('cachematrix.R')
+## Create a simple square matrix
 x<-rbind(c(1,2,3),c(0,1,5),c(5,6,0))
+## Call makeCacheMatrix and assign it to `inv` variable
 inv <- makeCacheMatrix(x)
+## And finally call cacheSolve
 cacheSolve(inv)
